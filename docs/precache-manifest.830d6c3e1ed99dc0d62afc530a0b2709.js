be550b7c88fd7763682bb49a803122dddb496c8f
Alfonso Zendejas <backdown.97@gmail.com>
2020-07-24 08:26:51 -0500

    Docs folder added

---
+++ docs/precache-manifest.830d6c3e1ed99dc0d62afc530a0b2709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117da4d8f5788e4ab994147da179c9c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a4b73cd49f612ca721c",
+    "url": "/static/css/main.e288a17c.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a4b73cd49f612ca721c",
+    "url": "/static/js/main.35c87bb0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
